dffaa60267166060cf3228c12dc9b7b97f9dc05a
John-bardera <johnbardera@gmail.com>
2020-11-03 18:50:14 +0900

    add dataloader & update train.py for ukiyoe

---
+++ dataLoader.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+import os
+import numpy as np
+
+
+class DataLoader(object):
+	def __init__(self, validation_size: float):
+		self._basename_list = [
+			'ukiyoe-train-imgs.npz',
+			'ukiyoe-train-labels.npz'
+		]
+		self.validation_size = validation_size
+
+	def load(self, data_path: str, random_seed: int=13) -> np.ndarray:
+		filenames_list = self._make_filenames(data_path)
+		data_list = [np.load(filename)['arr_0'] for filename in filenames_list]
+
+		all_images, all_labels = data_list
+
+		# shuffle data
+		np.random.seed(random_seed)
+		perm_idx = np.random.permutation(len(all_images))
+		all_images = all_images[perm_idx]
+		all_labels = all_labels[perm_idx]
+
+		# split train and validation
+		validation_num = int(len(all_labels) * self.validation_size)
+
+		validation_images = all_images[:validation_num]
+		validation_labels = all_labels[:validation_num]
+
+		train_images = all_images[validation_num:]
+		train_labels = all_labels[validation_num:]
+
+		return train_images, train_labels, validation_images, validation_labels
+
+	def _make_filenames(self, datapath: str) -> list:
+		filenames_list = [os.path.join(datapath, basename) for basename in self._basename_list]
+		return filenames_list
--- train.py
+++ train.py
@@ -1,12 +1,27 @@
 # coding: utf-8
 
+import time
+import numpy as np
 import tensorflow as tf
 from myModel import MyModel
+from dataLoader import DataLoader
 
 
 if __name__ == '__main__':
-	H, W, C = 28, 28, 1
-	(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+	# 4 mnist
+	# H, W, C = 28, 28, 1
+	# (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+	# 4 ukiyoe
+	DATA_PATH = './data/'
+	H, W, C = 224, 224, 3
+	RH, RW = 224, 224
+	x_train, y_train, x_test, y_test = DataLoader(0.2).load(DATA_PATH)
+	# x_train = tf.image.resize_with_pad(x_train, RH, RW)
+	# x_test = tf.image.resize_with_pad(x_test, RH, RW)
+	if C == 1:
+		x_train = np.sum(x_train, axis=-1) / 3
+		x_test = np.sum(x_test, axis=-1) / 3
 
 	x_train = x_train.astype('float32') / 255
 	x_test = x_test.astype('float32') / 255
@@ -48,13 +63,32 @@ if __name__ == '__main__':
 
 
 	def train(model, x_train, y_train, epochs, batch_size):
-
-		ds_train = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(batch_size)
+		ds_train = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(1000).batch(batch_size)
 		for epoch in range(epochs):
-			for images, labels in ds_train:
+			for batch_num, (images, labels) in enumerate(ds_train):
+				timef = time.time()
+				if H != RH:
+					images = resize(images)
 				train_step(images, labels)
-				tf.print(f'Epoch: {epoch + 1}, Loss: {train_loss.result()}, Accuracy: {train_accuracy.result() * 100}')
+				timet = time.time()
+				if batch_num % 10 == 0:
+					tf.print(
+						f'Epoch-Batch: {epoch + 1}-{batch_num + 1}, '
+						f'Time: {"{:.2f}".format(timet - timef)}s, '
+						f'Loss: {train_loss.result()}, '
+						f'Accuracy: {train_accuracy.result() * 100}'
+					)
+			test_step(x_test, y_test)
+			tf.print(
+				f'---Test Result---\n'
+				f'Epoch: {epoch + 1}, '
+				f'Loss: {test_loss.result()}, '
+				f'Accuracy: {test_accuracy.result() * 100}'
+			)
 		train_loss.reset_states()
 		train_accuracy.reset_states()
 
-	train(model, x_train, y_train, epochs=2, batch_size=32)
+	def resize(images):
+		return tf.image.resize(images, [RH, RW])
+
+	train(model, x_train, y_train, epochs=10, batch_size=32)
